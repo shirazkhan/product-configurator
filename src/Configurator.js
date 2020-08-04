985930c8b70f3d31b47a6241bb4acf817ae6a551
Shiraz Khan <sshirazz@gmail.com>
2020-08-04 16:17:56 +0100

    Add imgs to project

--- src/Configurator.js
+++ src/Configurator.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect} from 'react'
+import React, {useReducer} from 'react'
 import styled from 'styled-components';
 import Product from './Product';
 import Checkout from './Checkout';
